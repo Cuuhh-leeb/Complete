5cfc2a32ef3444f69ae21ace91a47625b6f7da44
Calieb Simpson <caliebsimpson@caliebs-mbp-2.attlocal.net>
2020-06-17 09:56:23 -0400

    complete

---
+++ src/Complete.java
@@ -0,0 +1,17 @@
+public class Complete {
+    public static void main(String[] args) {
+            int number = -75;
+
+
+            if (number > 0) {
+                System.out.println("Number is positive.");
+            }
+            else if (number < -1){
+                System.out.println("Number is not positive.");
+            }
+
+            System.out.println("Application finished.");
+        }
+    }
+
+
